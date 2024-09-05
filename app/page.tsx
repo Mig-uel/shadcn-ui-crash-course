0442391a7f35b4860be95d39baf7cf0a48820b40
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-04 20:02:45 -0400

    chore: Update Home page to display recipes using Card components and fetch data from mock API

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Sprout } from 'lucide-react'
+
 interface Recipe {
   title: string
   image: string
@@ -7,6 +18,39 @@ interface Recipe {
   id: string
 }
 
-export default function Home() {
-  return <main>home</main>
+async function getRecipes(): Promise<Recipe[]> {
+  return await (await fetch('http://localhost:5000/recipes')).json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes()
+
+  return (
+    <main>
+      <div className='grid grid-cols-3 gap-8'>
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className='flex flex-col justify-between'>
+            <CardHeader className='flex flex-row gap-4 items-center'>
+              {/* TODO: avatar */}
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className='flex justify-between gap-3'>
+              <Button>View Recipe</Button>
+              {recipe.vegan && (
+                <Button variant='ghost' size='icon'>
+                  <Sprout />
+                </Button>
+              )}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  )
 }
